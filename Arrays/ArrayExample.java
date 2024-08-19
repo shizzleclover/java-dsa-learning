cecdc49282f72037706c2872f3ecee57013e0c02
ShizzleClover <murewatajala@gmail.com>
2024-08-19 18:22:00 +0100

    Main comm

---
+++ Arrays/ArrayExample.java
@@ -0,0 +1,32 @@
+/* Arrays are collections of elements of the same type, stored in contiguous memory locations.
+   They have an initial index of 0, meaning if you are to count elements in an array, you start with 0, 1, 2, ..., instead of the usual 1, 2, 3. */
+
+/* To use an array, it has to be declared first in this format:
+   dataType[] arrayName; */
+
+/* Basically, arrays are used to store data. Imagine having to individually print out data for a farm and starting manually with an output statement.
+   It would be time-consuming, the code would be cumbersome and ambiguous, and errors would likely occur.
+   Instead, we can store all the data in an array. */
+   
+// Here is an example to illustrate the concept:
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Declare variables for each fruit
+        String apple = "Apple";
+        String banana = "Banana";
+        String mango = "Mango";
+        String cherry = "Cherry";
+        String apricot = "Apricot";
+        String pear = "Pear";
+
+        // Instead of using separate variables, use an array to store the fruit names
+        final String[] fruits = {apple, banana, mango, cherry, apricot, pear};
+
+        // Print each fruit in the array
+        // This is how you access and print array data
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println("Fruit at index " + i + ": " + fruits[i]);
+        }
+    }
+}
---
+++ Arrays/ArrayInitialization.java
@@ -0,0 +1,33 @@
+// ArrayInitialization.java
+// This class demonstrates different ways to initialize arrays in Java.
+
+public class ArrayInitialization {
+    public static void main(String[] args) {
+        // Inline initialization of an array
+        // The array 'inlineArray' is initialized with values at the time of declaration
+        int[] inlineArray = {1, 2, 3, 4, 5};
+
+        // Separate initialization of an array
+        // The array 'separateArray' is created with a specific size and then populated
+        int[] separateArray = new int[5]; // Array of size 5
+        for (int i = 0; i < separateArray.length; i++) {
+            separateArray[i] = i + 1; // Assign values to each index
+        }
+
+        // Print the arrays to demonstrate their contents
+        System.out.println("ArrayInitialization:");
+        System.out.print("Inline Initialization: ");
+        for (int num : inlineArray) {
+            // Print each element of the inlineArray
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        System.out.print("Separate Initialization: ");
+        for (int num : separateArray) {
+            // Print each element of the separateArray
+            System.out.print(num + " ");
+        }
+        System.out.println("\n");
+    }
+}
