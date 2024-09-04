f0d43e0f4c5bd75038231068a49ba275f93b58db
ShizzleClover <murewatajala@gmail.com>
2024-09-04 17:53:50 +0100

    Worked on Control_Strucutures folder

---
+++ Crash-Course/Control_Structures/Loops/for_loop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class for_loop {
+    public static void main(String[] args) {
+        int [] age = {1, 2, 3, 4, 5, 6};
+
+        for(int i = 0; i > age.length; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
